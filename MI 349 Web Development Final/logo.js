650908e77de1902000f7df227f17afc20546b68a
Barry Pettway <barrypettway@yahoo.com>
2015-12-11 23:27:12 -0500

    BDPWebsite
    Final Project website

---
+++ MI 349 Web Development Final/logo.js
@@ -0,0 +1,19 @@
+
+
+
+function mouseOver() {
+document.getElementById("trav").style.color = "green";
+}
+function mouseOut() {
+document.getElementById("trav").style.color = "black";
+}
+
+//function write () {
+  //var randomcolor;
+  //$('.trav').on('click', function(){
+   //randomcolor = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+  //  $(this).toggleClass('active');
+  //  $(this).css('background', randomcolor);
+  //});
+
+//}
